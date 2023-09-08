3c03ee0f4644b7fbd69073a294f697c9cc1c83c3
pbl.gllgs <pbl.gllgs@gmail.com>
2023-09-08 19:40:20 -0300

    docker compose

--- src/main/java/com/pblgllgs/spring/security/SecurityApplication.java
+++ src/main/java/com/pblgllgs/spring/security/SecurityApplication.java
@@ -10,13 +10,15 @@ import com.pblgllgs.spring.security.user.Role;
 import com.pblgllgs.spring.security.user.User;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 import java.util.Random;
@@ -26,63 +28,72 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class SecurityApplication {
 
-	private final UserRepository userRepository;
-	private static final Random RANDOM = new Random();
+    private static final Random RANDOM = new Random();
+    private final UserRepository userRepository;
+    @Value("#{'${cors.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+    @Value("#{'${cors.allowed-methods}'.split(',')}")
+    private List<String> allowedMethods;
+    @Value("#{'${cors.allowed-headers}'.split(',')}")
+    private List<String> allowedHeaders;
+    @Value("#{'${cors.exposed-headers}'.split(',')}")
+    private List<String> exposedHeaders;
 
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    private static void generateCustomers(int quantity, CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        for (int i = 0; i < quantity; i++) {
+            Faker faker = new Faker();
+            Name name = faker.name();
+            String firstName = name.firstName();
+            String lastName = name.lastName();
+            int age = RANDOM.nextInt(1, 100);
+            Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+            Customer customer = new Customer(
+                    firstName + " " + lastName,
+                    firstName.toLowerCase() + "." + lastName.toLowerCase() + "@gmail.com",
+                    passwordEncoder.encode(UUID.randomUUID().toString()), age,
+                    gender);
+            customerRepository.save(customer);
+        }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedMethods("*")
-						.allowedOrigins("http://localhost:5173/");
-			}
-		};
-	}
+    }
 
-	@Bean
-	CommandLineRunner runner(
-			CustomerRepository customerRepository,
-			PasswordEncoder passwordEncoder
-	) {
-		return args -> {
-			generateCustomers(10,customerRepository, passwordEncoder);
-			User user = User.builder()
-					.firstName("user")
-					.lastName("user")
-					.email("pbl.gllgs@gmail.com")
-					.password(passwordEncoder.encode("12345"))
-					.role(Role.USER)
-					.build();
-			userRepository.save(user);
-		};
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
-	private static void generateCustomers(int quantity, CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
-		for (int i = 0; i < quantity; i++) {
-			Faker faker = new Faker();
-			Name name = faker.name();
-			String firstName = name.firstName();
-			String lastName = name.lastName();
-			int age = RANDOM.nextInt(1, 100);
-			Gender gender = age %2 == 0 ? Gender.MALE : Gender.FEMALE;
-			Customer customer = new Customer(
-					firstName + " " +lastName,
-					firstName.toLowerCase() + "." + lastName.toLowerCase() + "@gmail.com",
-					passwordEncoder.encode(UUID.randomUUID().toString()), age,
-					gender);
-			customerRepository.save(customer);
-		}
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(allowedMethods);
+        configuration.setAllowedHeaders(allowedHeaders);
+        configuration.setExposedHeaders(exposedHeaders);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
+    }
 
-	}
+    @Bean
+    CommandLineRunner runner(
+            CustomerRepository customerRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        return args -> {
+            generateCustomers(10, customerRepository, passwordEncoder);
+            User user = User.builder()
+                    .firstName("user")
+                    .lastName("user")
+                    .email("pbl.gllgs@gmail.com")
+                    .password(passwordEncoder.encode("12345"))
+                    .role(Role.USER)
+                    .build();
+            userRepository.save(user);
+        };
+    }
 
 }
