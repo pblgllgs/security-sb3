28e4ee4824877a2963f56877122553acb4f5d9d8
pbl.gllgs <pbl.gllgs@gmail.com>
2023-09-08 21:11:22 -0300

    docker compose add actuator check health

--- src/main/java/com/pblgllgs/spring/security/config/SecurityConfiguration.java
+++ src/main/java/com/pblgllgs/spring/security/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.pblgllgs.spring.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,6 +28,8 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests()
                 .requestMatchers("/api/v1/auth/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET,"/actuator/health")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
